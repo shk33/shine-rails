f69203c5b5e280e68bf86281109681026fdd8d08
Miguel Coronel <m.coronel.seg@gmail.com>
2016-07-22 02:31:51 -0500

    Implements paginated search with Angular

--- app/assets/javascripts/customer_app.js
+++ app/assets/javascripts/customer_app.js
@@ -9,16 +9,38 @@ app.controller('CustomerSearchController', [
     '$scope',
     '$http', 
     function ($scope, $http) {
+      $scope.page = 0;
       $scope.customers = [];
 
-      $scope.search = function (searchTerm) {
+      $scope.search = function (searchTerm, isNew) {
+        if (searchTerm.length < 3) {
+          return;
+        }
+
+        if (isNew) {
+          $scope.page = 0;
+        }
+
         $http.get("/customers.json", 
-          {"params": {"keywords": searchTerm}}
+          {"params": {"keywords": searchTerm, "page": $scope.page}}
           ).then(function (response) {
             $scope.customers = response.data;
           }, function (response) {
             alert('There was a problem ' + response.status);
           });
       }
+
+      $scope.previousPage = function () {
+        if ($scope.page > 0) {
+          $scope.page = $scope.page -1;
+          $scope.search($scope.keywords, false);
+        }
+      }
+
+      $scope.nextPage = function () {
+        $scope.page = $scope.page + 1;
+        $scope.search($scope.keywords, false);
+      }
+
     }
 ]);
